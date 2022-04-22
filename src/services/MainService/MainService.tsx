0efb579666b4a2567014f932431a3efacbba86f3
tungnt16 <tungnt16@vng.com>
2022-04-22 23:48:36 +0700

    update src

---
+++ src/services/MainService/MainService.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const MainService: React.FC = () => {
+  // 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
+  return <div>MainService</div>;
+};
+
+export default MainService;
