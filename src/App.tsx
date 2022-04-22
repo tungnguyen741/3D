8ba5f0a9e19b60d0e738914da9ece6fe9dfa1a58
tungnt16 <tungnt16@vng.com>
2022-04-23 04:02:53 +0700

    push

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,50 @@
-import React from "react";
-import MainService from "src/services/MainService";
+import { Canvas } from "@react-three/fiber";
+import * as React from "react";
+import Box from "src/components/Box";
 import "./App.css";
 
 const App = () => {
-  return <MainService />;
+  return (
+    <Canvas resize={{ scroll: true, offsetSize: true }}>
+      <ambientLight intensity={1} />
+      <spotLight position={[10, 10, 10]} angle={2} penumbra={1} />
+      <pointLight position={[-10, -10, -10]} />
+
+      {/* T */}
+      <Box position={[-7, 3, -3]} scale={1} />
+      <Box position={[-7, 1, -3]} scale={1} />
+      <Box position={[-7, -1, -3]} scale={1} />
+
+      <Box position={[-9, 3, -3]} scale={1} />
+      <Box position={[-5, 3, -3]} scale={1} />
+      {/* Ù */}
+      <Box position={[-2, 3, -3]} scale={1} />
+      <Box position={[-2, 1, -3]} scale={1} />
+      <Box position={[-2, -1, -3]} scale={1} />
+
+      <Box position={[0, -1, -3]} scale={1} />
+      <Box position={[2, -1, -3]} scale={1} />
+
+      <Box position={[2, 1, -3]} scale={1} />
+      <Box position={[2, 3, -3]} scale={1} />
+      {/* N */}
+      <Box position={[5, 3, -3]} scale={1} />
+      <Box position={[5, 1, -3]} scale={1} />
+      <Box position={[5, -1, -3]} scale={1} />
+
+      <Box position={[7, 1.75, -3]} scale={1} />
+      <Box position={[8, -0.1, -3]} scale={1} />
+      <Box position={[9, -1, -3]} scale={1} />
+
+      <Box position={[9, 1, -3]} scale={1} />
+      <Box position={[9, 3, -3]} scale={1} />
+
+      <Box position={[-1, 5.5, -3]} scale={1} />
+      <Box position={[0.35, 5.25, -3]} scale={1} />
+      <Box position={[1.7, 4.85, -3]} scale={1} />
+      {/* <Box position={[0.45, 4, -3]} scale={1} /> */}
+    </Canvas>
+  );
 };
 
 export default App;
--- src/services/MainService/MainService.tsx
+++ src/services/MainService/MainService.tsx
@@ -1,41 +1,7 @@
-import React, { useEffect } from "react";
-import * as THREE from "three";
+import React from "react";
 
 const MainService: React.FC = () => {
-  useEffect(() => {
-    const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera(
-      50,
-      window.innerWidth / window.innerHeight,
-      0.1,
-      2000
-    );
-
-    const renderer = new THREE.WebGLRenderer();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    renderer.setClearColor(12345679101112135);
-    document.body.appendChild(renderer.domElement);
-
-    const geometry = new THREE.BoxGeometry();
-    const material = new THREE.MeshBasicMaterial({ color: 1021309 });
-    const cube = new THREE.Mesh(geometry, material);
-    scene.add(cube);
-
-    camera.position.z = 5;
-
-    function animate() {
-      requestAnimationFrame(animate);
-
-      cube.rotation.x += 0.02;
-      cube.rotation.y += 0.02;
-
-      renderer.render(scene, camera);
-    }
-
-    animate();
-  }, []);
-
-  return <div></div>;
+  return <div>'1'</div>;
 };
 
 export default MainService;
