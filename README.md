0663dade803058df31d299a356a428ab40b88dea
tungnt16 <tungnt16@vng.com>
2022-04-23 02:47:18 +0700

    rebuild

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-# Getting Started with Create React App123123
+![CI/CD](https://github.com/dyarleniber/react-workflow-gh-actions/workflows/CI/CD/badge.svg)
+
+# Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
--- src/services/MainService/MainService.tsx
+++ src/services/MainService/MainService.tsx
@@ -1,8 +1,41 @@
-import React from "react";
+import React, { useEffect } from "react";
+import * as THREE from "three";
 
 const MainService: React.FC = () => {
-  // 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
-  return <div>QQJZ MÁ</div>;
+  useEffect(() => {
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(
+      50,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      2000
+    );
+
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setClearColor(1234567910111213);
+    document.body.appendChild(renderer.domElement);
+
+    const geometry = new THREE.BoxGeometry();
+    const material = new THREE.MeshBasicMaterial({ color: 1021309 });
+    const cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
+
+    camera.position.z = 5;
+
+    function animate() {
+      requestAnimationFrame(animate);
+
+      cube.rotation.x += 0.02;
+      cube.rotation.y += 0.02;
+
+      renderer.render(scene, camera);
+    }
+
+    animate();
+  }, []);
+
+  return <div></div>;
 };
 
 export default MainService;
